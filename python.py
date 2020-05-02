f5048c97f1417ae59fc1ac8a3681f6eafef39128
Xand <pythonproger@yandex.ru>
2020-05-03 02:21:27 +0500

    Add python.py

--- python.py
+++ python.py
@@ -1,12 +1,20 @@
 #git
-
 from tkinter import *
-from tkinter import ttk
 
+root = Tk()
+root.geometry("650x370+350+150")
+# Не получилось найти ico картинку для логотипа, поставь пожалуйста
+root.title("Test")
+root.resizable(False, False)
+root.config(bg = "light green")
 
-root = Tk(")
+btn = Button(root, text ="Button", bg ="#196", bd = 8)
+btn.pack()
 
-root.mainloop
+btn2 = Button(root, text ="Button2", bg = "#195",bd = 6)
+btn2.pack()
+
+root.mainloop()
 
 
 
